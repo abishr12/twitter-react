54ba512a6006e16f4d19edaebdae178c465966bc
adham bishr <adhambishr@gmail.com>
2017-11-18 14:13:42 -0800

    creation

---
+++ model/keys.js
@@ -0,0 +1,10 @@
+//console.log("this is loaded");
+
+//Twitter API Authentication
+
+var twitterKeys = {
+  consumer_key: "wdDTwyDIcJCLqvoFXVFBthwmK",
+  consumer_secret: "TuEFPCBwZQHviBSjKYGaHPigh8m9IHa1non9oIkVYneaOur9Rt",
+  access_token_key: "919313156471529473-zKcb5AZlq1jqaFWq2qpwSnmxdF6EOUX",
+  access_token_secret: "Ca9ItYvV0ikN1sROr3bWurT48O7Ex0qz4feu05EF3FlY0"
+};
